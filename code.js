e8581a4d9c1ff46eaeed7a19df53b5ae4718c950
Vianey Villalobos <vvillalobos-zuniga14@smpanthers.org>
2014-05-13 10:48:24 -0700

    o

---
+++ code.js
@@ -0,0 +1,14 @@
+function himine() {
+	$("#mine").html("I actually don't own any pets.");
+        $("#mine").show();
+        }
+      
+function changeColor() {
+        //changes header to red and add border
+        $(".one").css("color", "white");
+        }
+
+function changeColor2() {
+        //changes header to red and add border
+        $(".one").css("color", "blue");
+        }
