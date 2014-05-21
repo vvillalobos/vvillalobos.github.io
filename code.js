78afd480b7ec8073c78510fe86854b4ffe4d5075
Vianey Villalobos <vvillalobos-zuniga14@smpanthers.org>
2014-05-21 13:06:34 -0700

    o

--- code.js
+++ code.js
@@ -12,3 +12,18 @@ function changeColor2() {
         //changes header to red and add border
         $(".one").css("color", "blue");
         }
+function game() {	
+		var number = Math.floor(Math.random()*11)
+                var a = prompt("Welcome to the Guessing Game! Guess a number between 1-10");
+                while (a != number) {
+                   if ( a < number) {
+                        var a = prompt("Guess too low. Guess again!");
+                   }
+                   else {
+                        var a = prompt("Guess too high. Guess again!");
+                   }
+                };
+                if ( a == number) {
+                        alert("YOU WON.");
+                }
+}
